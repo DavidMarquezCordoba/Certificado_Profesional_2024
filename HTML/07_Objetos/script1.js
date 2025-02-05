d4cfe88593ee7287ccc6ab63cec5f8db84164f2a
DavidMarquezCordoba <101516673+DavidMarquezCordoba@users.noreply.github.com>
2025-02-05 14:05:17 +0100

    Apuntes de Accesibilidad y modificación del ejercicio 7 (objetos)

--- HTML/07_Objetos/script1.js
+++ HTML/07_Objetos/script1.js
@@ -39,3 +39,79 @@ console.log("Mi objeto actualmente congelado: ", miObjeto);
 // Conclusión: Ni se añade "abierta" ni modifica el color
 
 delete miObjeto.tamano; //Para que funcione, debe comentarse frezee y seal
+
+
+// DESTRUCTURING
+console.log("////////////Destructuring\\\\\\\\\\\\\\\\\\\\\\");
+const persona = {
+   nombre: "pepe",
+   edad: 30,
+   pais: "España"
+}
+
+// Forma clásica de crear variables con propiedades de un obj
+// let nombre = persona.nombre;
+// let edad = persona.edad;
+
+// Con Destructuring
+let {nombre, edad, pais} = persona;
+
+console.log("nombre: ",nombre);
+console.log("edad: ",edad);
+console.log("pais: ",pais);
+
+// Métodos en objetos
+console.log("************************");
+console.log("Métodos en objetos");
+console.log("==================");
+
+const miListaCanciones = ["cancion1","cancion2","cancion3","cancion4","cancion5",]
+
+const reproductor = {
+   lista: [...miListaCanciones, "cancion6"],
+   volumen: 80,
+   cancion: "",
+   reproducir: function(numCancion) {
+      if (numCancion >= 0 && numCancion<this.lista.length) {
+         this.cancion = this.lista[numCancion];
+         console.log(`Reproduciendo la canción: ${this.cancion}`);
+         return "reproduciendo"
+      } else return "Fallo al reproducir"
+   },
+
+   parar: function() {
+      this.cancion = "";
+      console.log("Parado el reproductor");
+      
+   }
+};
+
+console.log("Lista: ",reproductor.lista);
+console.table("Canción: ",reproductor.cancion);
+
+// Le decimos que reproducza la canción en la posición 2
+let estadoReproductor = reproductor.reproducir(2)
+
+console.log("Estado: ", estadoReproductor);
+console.table(reproductor.cancion)
+
+estadoReproductor = reproductor.reproducir(9);
+console.log("Estado: ", estadoReproductor);
+
+reproductor.parar();
+
+// Reproductores
+console.log("Reproductores");
+const reproductores = [];
+
+for (let i = 0; i < 5; i++) {
+   reproductores.push({...reproductor});
+}
+
+console.table(reproductores);
+reproductores[2].reproducir(1);
+reproductores[1].reproducir(4);
+console.table(reproductores);
+
+
+
