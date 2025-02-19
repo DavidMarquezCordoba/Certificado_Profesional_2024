65f27c04bc9f9f089c4fd54151641a3d9c0b3cf6
DavidMarquezCordoba <101516673+DavidMarquezCordoba@users.noreply.github.com>
2025-02-19 10:00:01 +0100

    Cambio de ruta, reesctructuración

--- HTML/0_D/js/script.js
+++
@@ -1,7 +0,0 @@
-let padre = document.querySelector(".tareas");
-let botonAgregar = document.querySelector("#boton-agregar");
-
-// 1º LocalStorage
-let entradas = JSON.parse(localStorage.getItem("entradas")) || [];
-
-
